c47fd0240a08d541a9314760c4f5cd1a13f07f20
Furkan Sayyed <73273662+Furkansayyed@users.noreply.github.com>
2023-10-24 19:06:36 +0530

    Update app.py
    Added all the features as per requirements.

--- task_4/app.py
+++ task_4/app.py
@@ -3,10 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.secret_key = 'secret_key'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+    def __init__(self, email, password, username):
+        self.email = email
+        self.username = username
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
@@ -36,6 +53,30 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        errors = []
+        email = request.form['email']
+        password = request.form['password']
+
+        if not email:
+            errors.append('Please enter the email')
+
+        if not password:
+            errors.append('please enter password')
+
+        if errors:
+            return render_template('login.html', errors=errors)
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.check_password(password):
+            session['username'] = user.username
+            session['email'] = user.email
+            session['password'] = user.password
+            return redirect('/dashboard')
+        else:
+            errors.append('Invalid Login Credentials')
+            return render_template('login.html', errors=errors)
     return render_template('login.html')
 
 
@@ -46,6 +87,6 @@ def dashboard():
         return render_template('dashboard.html', user=user)
     
     return redirect('/login')
-  
+
 if __name__ == '__main__':
     app.run(debug=True)
